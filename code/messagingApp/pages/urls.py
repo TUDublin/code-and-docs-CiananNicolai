103fe6a4c5df07a4511a1c6e0db1aa9fe72b30c1
X00160711 Cianan Nicolai <X00160711@ta.it-tallaght.ie>
2022-10-21 12:42:04 +0100

    IP code implemented but not working for unknown reasons, may be college firewall.

--- code/messagingApp/pages/urls.py
+++ code/messagingApp/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, show_ip_address
+from pages.views import show_ip_address
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('user_ip/',show_ip_address),
     ]
--- code/messagingApp/pages/views.py
+++ code/messagingApp/pages/views.py
@@ -1,4 +1,18 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def get_ip_address(request):
+    user_ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+    if user_ip_address:
+        ip = user_ip_address.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+def show_ip_address(request):
+    user_ip = get_ip_address(request)
+    return render(request, "home.html", {"user_ip":user_ip})
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
