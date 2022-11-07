cfba7db71967bd386bbb2c0f22d3123affafc9d3
X00160711 Cianan Nicolai <X00160711@ta.it-tallaght.ie>
2022-11-07 11:39:27 +0000

    post database setup in django

--- code/messagingApp/pages/admin.py
+++ code/messagingApp/pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserPost, PostType
 
-# Register your models here.
+class PostTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(PostType, PostTypeAdmin)
+
+class UserPost(admin.ModelAdmin):
+    list_display = ['text', 'image']
+
+admin.site.register(UserType, UserPostAdmin)
--- code/messagingApp/pages/models.py
+++ code/messagingApp/pages/models.py
@@ -1,3 +1,48 @@
+import uuid
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class PostType(models.Model):
+        id = models.UUIDField(
+        primary_key=True
+        default=uuid.uuid4
+        editable=False
+    )
+    name = models.CharField(max_length=250, unique=True)
+
+    class Meta:
+        ordering =('name',)
+        verbose_name = 'type'
+        verbose_name_plural ="types"
+    
+    def get_absolute_url(self):
+        return reverse('pages:posts_by_type', args=[self.id])
+
+    def __str__(self):
+        return self.name
+
+
+class UserPost(models.Model):
+    id = models.UUIDField(
+        primary_key=True
+        default=uuid.uuid4
+        editable=False
+    )
+    text = models.CharField(max_length=250, unique=True)
+    image = models.ImageField(upload_to="userPost", blank=True)
+    likes = models.DecimalField(black=True)
+    comments = models.DecimalField(black=True)
+    postTime = models.DateTimeField(auto_now_add=True, black=True, null=True)
+    edited = models.DateTimeField(auto_now_add=True, black=True, null=True)
+
+    class Meta:
+        ordering =('text',)
+        verbose_name = 'post'
+        verbose_name_plural ="posts"
+    
+    def get_absolute_url(self):
+        return reverse('pages:post detail', args=[self.posttype.id, self.id])
+
+    def __str__(self):
+        return self.name
