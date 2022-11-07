32d833fb0d4e98f296e60e377e4a1bed3e5100b3
X00160711 Cianan Nicolai <X00160711@ta.it-tallaght.ie>
2022-11-07 11:04:07 +0000

    post layout and images.

--- code/messagingApp/messagingApp/settings.py
+++ code/messagingApp/messagingApp/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_FINDERS = ["django.contrib.staticfiles.finders.FileSystemFinder","django.contrib.staticfiles.finders.AppDirectoriesFinder"]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
