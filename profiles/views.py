75bb087794c41115b5f19b93d73513565b437ce0
Cianan Nicolai <cianannicolai@gmail.com>
2023-04-07 15:31:33 +0100

    Profiles working, needs UI.

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Profile
+from pages.models import UserPost
 
 @receiver(post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
@@ -18,5 +19,7 @@ def profile_view(request, username):
     User = get_user_model()
     user = get_object_or_404(User, username=username)
     profile = get_object_or_404(Profile, username_id=user)
-    context = {'profile': profile}
-    return render(request, 'profiles/profile.html', context)
+    post_list = UserPost.objects.filter(username=user)
+    context = {'user': user, 'profile': profile, 'post_list': post_list}
+    return render(request, 'profile.html', context)
+
